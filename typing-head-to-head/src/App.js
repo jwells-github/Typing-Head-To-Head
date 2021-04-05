ac3088e4bdda576d65e20aae69ac4beee0c6f8de
JakeW <jakewellsd@gmail.com>
2021-04-05 17:00:07 +0100

    implement matching of user input to word

--- typing-head-to-head/src/App.js
+++ typing-head-to-head/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      words: ["The","big","dog","named","Dave","ate","sausages!"],
+      currentPosition : 0
+    } 
+  }
+
+  compareInput(event){
+    let input = event.target.value
+    let inputChar = input.substring(event.target.value.length-1,input.length)
+    let currentWord = this.state.words[this.state.currentPosition]
+
+    if(inputChar === " "){
+      if(input.substring(0,input.length-1) === this.state.words[this.state.currentPosition]){
+        this.setState({currentPosition: this.state.currentPosition +1})
+        event.target.value = ""
+        event.target.style.background = "white"
+        return
+      }
+    }
+    if (currentWord.substring(0,input.length) !== input){
+      event.target.style.background = "red"
+    }
+    else{
+      event.target.style.background = "white"
+    }
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <div>
+          <h1>{this.state.words[this.state.currentPosition]}</h1>
+        </div>
+        <div>
+          <input onInput={this.compareInput.bind(this)}></input>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
