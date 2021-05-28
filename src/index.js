5bbccae081920d4b3514f6685bfe38245eb50784
JakeW <jakewellsd@gmail.com>
2021-05-28 10:39:13 +0100

    add page title

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <title>Typing Head-to-head</title>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
